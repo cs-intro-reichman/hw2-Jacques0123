ae9813492ce88594c27a149898c4c8ac94b70427
Jacques0123 <jacques.treveza@post.runi.ac.il>
2024-11-13 11:17:35 +0200

    trying first part of hw

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,27 @@
 // Computes an approximation of PI.
+
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+
+    public static void main(String[] args) {
+        int decimalFigures = Integer.parseInt(args[0]);
+        double piApproximation = 0.0;
+
+        for (int i = 0; i < decimalFigures; i++) {
+            if (i % 2 == 0) {
+                piApproximation += 1.0 / (2 * i + 1);
+            } else {
+                piApproximation -= 1.0 / (2 * i + 1);
+            }
+        }
+
+        piApproximation *= 4.0;
+
+        String realPiLabel = "pi according to Java:";
+        String approxPiLabel = "pi, approximated:";
+        int padding = realPiLabel.length() - approxPiLabel.length();
+        String spaces = " ".repeat(Math.max(0, padding));
+
+        System.out.printf("%s %.15f%n", realPiLabel, Math.PI);
+        System.out.printf("%s%s %.15f%n", approxPiLabel, spaces, piApproximation);
+    }
 }
--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,28 @@
-// Prints a crowd cheering output.
+
 public class Cheers {
-        public static void main(String[] args) {
-	    // Replace this comment with your code
+
+    public static void main(String[] args) {
+        String cheer = args[0];
+        int times = Integer.parseInt(args[1]);
+
+        cheer = cheer.toUpperCase();
+
+        String vowels = "AEFHILMNORSX";
+
+        for (int i = 0; i < cheer.length(); i++) {
+            char letter = cheer.charAt(i);
+
+            if (vowels.indexOf(letter) != -1) {
+                System.out.println("Give me an " + letter + ": " + letter + "!");
+            } else {
+                System.out.println("Give me a " + letter + ": " + letter + "!");
+            }
         }
+
+        System.out.println("What does that spell?");
+
+        for (int i = 0; i < times; i++) {
+            System.out.println(cheer + "!!!");
+        }
+    }
 }
